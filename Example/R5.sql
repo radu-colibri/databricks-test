8b0dfa568606ab10208b632d9bfe47a1aaf0f537
radu.nicolau@colibridigital.io <radu.nicolau@colibridigital.io>
2024-09-22 16:59:46 +0000

    jobs list

--- Example/R5.sql
+++ Example/R5.sql
@@ -36,8 +36,13 @@ INSERT INTO jobs
 VALUES
 --(1, 'Mobile Augmented Reality Developer', 1, date('2012-05-01'), date('2018-04-01'));
 --(2, 'Lead Engineer / Developer Relations', 1, date('2014-05-01'), date('2018-04-01'))
-(3, 'UX/UI Designer', 2, date('2018-04-01'), date('2018-10-01')),
-(4, 'UX UI Frontend Engineer', 3, date('2018-10-01'), date('2019-10-01'))
+--(3, 'UX/UI Designer', 2, date('2018-04-01'), date('2018-10-01')),
+--(4, 'UX UI Frontend Engineer', 3, date('2018-10-01'), date('2019-10-01')),
+(5, 'Senior Frontend Engineer', 4, date('2019-10-01'), date('2021-02-01')),
+(6, 'Senior Frontend Engineer', 5, date('2021-02-01'), date('2021-04-01')),
+(7, 'Senior Frontend Engineer', 6, date('2021-04-01'), date('2021-08-01')),
+(8, 'Senior Frontend Engineer', 7, date('2021-08-01'), null),
+(9, 'Senior Marketing Designer', 7, date('2023-08-01'), null);
 
 
 -- COMMAND ----------
@@ -47,8 +52,13 @@ ORDER BY id;
 
 -- COMMAND ----------
 
---Get all comapnies that have jobs associated
-SELECT jobs.title AS `Job title`, companies.name AS `Company`
+-- List of all jobs and respective companies, with Present as the end date for current jobs 
+SELECT
+jobs.title AS `Job title`,
+companies.name AS `Company`,
+jobs.start_date AS `Start date`,
+coalesce(jobs.end_date, 'Present') AS `End date`,
+DATEDIFF(month, jobs.start_date, coalesce(jobs.end_date, GETDATE())) as `Length (months)`
 FROM jobs
-INNER JOIN companies ON jobs.company_id = companies.id
-ORDER BY jobs.id
+LEFT JOIN companies ON jobs.company_id = companies.id
+ORDER BY DATEDIFF(month, jobs.start_date, coalesce(jobs.end_date, GETDATE())) DESC
